0e79c8e0899f262e2a6a604e8c32dbf782db1649
NoXVSH <469677147@qq.com>
2020-10-23 16:58:44 +0800

    屏蔽代码

--- assets/script/core/loader/QueueLoader.ts
+++ assets/script/core/loader/QueueLoader.ts
@@ -1,38 +1,38 @@
-import LoaderManager from "./LoaderManager";
-import QueueLoaderBase, { QueueLoaderItem } from "./QueueLoaderBase";
-import { BundleName } from "./LoaderConst";
+// import LoaderManager from "./LoaderManager";
+// import QueueLoaderBase, { QueueLoaderItem } from "./QueueLoaderBase";
+// import { BundleName } from "./LoaderConst";
 
-export default class QueueLoader extends QueueLoaderBase {
-    private static _instance: QueueLoader;
+// export default class QueueLoader extends QueueLoaderBase {
+//     private static _instance: QueueLoader;
 
-    static get Instance(): QueueLoader {
-        if (this._instance == null) {
-            this._instance = new QueueLoader();
-        }
+//     static get Instance(): QueueLoader {
+//         if (this._instance == null) {
+//             this._instance = new QueueLoader();
+//         }
 
-        return this._instance;
-    }
+//         return this._instance;
+//     }
 
-    load(url: string, bundleName: BundleName, type: string | typeof cc.Asset, callback: Function, errorback?: Function) {
-        let tick = this.tick++;
+//     load(url: string, bundleName: BundleName, type: string | typeof cc.Asset, callback: Function, errorback?: Function) {
+//         let tick = this.tick++;
 
-        let queueItem: QueueLoaderItem = {
-            url: url, 
-            type: type, 
-            callback: callback, 
-            errorback: errorback, 
-            isLoading: false, 
-            timeOutTick: null, 
-            tick: tick,
-            bundleName : bundleName,
-            loader : LoaderManager.Instance.getBundleLoader(bundleName)
-        };
+//         let queueItem: QueueLoaderItem = {
+//             url: url, 
+//             type: type, 
+//             callback: callback, 
+//             errorback: errorback, 
+//             isLoading: false, 
+//             timeOutTick: null, 
+//             tick: tick,
+//             bundleName : bundleName,
+//             loader : LoaderManager.Instance.getBundleLoader(bundleName)
+//         };
 
-        this.waitList.push(queueItem);
-        this.checkLoadList();
+//         this.waitList.push(queueItem);
+//         this.checkLoadList();
 
-        return tick;
-    }
-}
+//         return tick;
+//     }
+// }
 
-window.regVar("QueueLoader", QueueLoader);
+// window.regVar("QueueLoader", QueueLoader);
